8b1e968a9cc042d3da3f5e859dc03e5842565be2
emamjion <emamjion@gmail.com>
2024-06-03 19:29:35 +0600

    vercel setup done

--- index.js
+++ index.js
@@ -28,16 +28,43 @@ const client = new MongoClient(uri, {
 }
 });
 
+
 async function run() {
     try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+
+    // COLLECTIONS
+    const projectsCollection = client.db('emamDb').collection('projects');
+
+    /* ------------------------------------- Crud Opereation code Here -------------------------------- */
+    
+    app.get('/projects', async(req, res) => {
+        const result = await projectsCollection.find().toArray();
+        res.send(result);
+    });
+
+    app.post('/projects', async(req, res) => {
+        const project = req.body;
+        const result = await projectsCollection.insertOne(project);
+        res.send(result);
+    })
+
+
+    /* ------------------------------------- Crud Opereation code Here -------------------------------- */
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
 } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
 }
 }
 run().catch(console.dir);
